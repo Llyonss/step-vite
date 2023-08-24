157e29edf54dc212842c0382a728048740d7f3d5
xiaoliangou <xiaoliangou@tencent.com>
2023-08-24 23:09:27 +0800

    feat: step-03 使用vue(NPM版)

---
+++ step-03/demo/App.js
@@ -0,0 +1,8 @@
+import { createApp } from './node_modules/vue/dist/vue.esm-browser.js'
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}).mount('#app')
---
+++ step-03/readme.md
@@ -0,0 +1,3 @@
+## 目标：
+1. 初始化npm 并 install vue
+2. 使用node_module里面的vue, 替换掉app.js的CDN
